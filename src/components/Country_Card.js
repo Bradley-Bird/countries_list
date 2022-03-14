b0f49d6391615da92b0b9501ace1ecb2e6cc09fa
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-14 16:42:25 -0700

    all names and flags rendering to the view

--- src/components/Country_Card.js
+++ src/components/Country_Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Country_Card({ name, ISO3 }) {
+export default function Country_Card({ name, iso2 }) {
   return (
     <div className="country">
       <p>{name}</p>
-      <img src={ISO3}></img>
+      <img src={`https://flagcdn.com/72x54/${iso2.toLowerCase()}.png`} />
     </div>
   );
 }
