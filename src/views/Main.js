19eb1ed241686a90ee3538e884711abb8d4b9748
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-14 15:27:12 -0700

    useEffect, is set up to grab the countries array from supabase

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchCountries } from '../services/countries';
 
 export default function Main() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchCountries();
+      console.log('resp', resp);
+    };
+    fetchData();
+  }, []);
   return <div>Main</div>;
 }
