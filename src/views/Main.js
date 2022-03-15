2f8df9467c9b47b092cef16321c58d4e2320c09a
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-15 15:07:27 -0700

    added loader

--- src/views/Main.js
+++ src/views/Main.js
@@ -2,24 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { fetchCountries } from '../services/countries';
 import Country_Card from '../components/Country_Card';
 import Dropdown from '../components/Dropdown';
+import './Main.css';
 
 export default function Main() {
   const [countries, setCountries] = useState([]);
   const [continents, setContinents] = useState([]);
   const [continent, setContinent] = useState('All');
   const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const resp = await fetchCountries();
       {
         try {
+          // I realized after doing all the research for this that it was totally unnecessary since we only have 7 things to choose from
           //make an array with the continents only once
           const onlyContinents = [...new Set(resp.map((aContinent) => aContinent.continent))];
           //filter out the null values and add All to the start
           const filteredContinents = onlyContinents.filter(Boolean);
           filteredContinents.unshift('All');
           setContinents(filteredContinents);
+          setLoading(false);
         } catch (e) {
           setErrorMessage('An error has occurred, please reload the page. ');
         }
@@ -33,6 +37,13 @@ export default function Main() {
   const filterCountries = () => {
     return countries.filter((country) => country.continent === continent || continent === 'All');
   };
+  //if loading = true then display loader
+  if (loading)
+    return (
+      <div>
+        <span className="loader"></span>
+      </div>
+    );
 
   return (
     <>
